5387a61c8c4d6fc28f144b87294c98e1ef03b3fe
Fherder <119635204+Fherder@users.noreply.github.com>
2024-11-12 19:18:07 -0300

    Quite comentarios y arregle merge

--- depuracion.py
+++ depuracion.py
@@ -4,7 +4,7 @@ import pandas as pd
 def depurar_archivo(file_path):
     """Function to clean and process the uploaded .log file."""
     try:
-        '''
+    
         # Assuming your .log file can be read with Pandas
         # marcaje = pd.read_csv(file_path, header= None, sep=',', usecols=[0, 2, 3, 5, 6], names=["Codigo", "entrada/salida", "rut", "hora", "minuto"])  
 
@@ -26,31 +26,22 @@ def depurar_archivo(file_path):
 
         data = marcaje
         data = data.dropna()
+
         # Return processed data or save it to a new file
-        processed_file_path = file_path.replace(".log", "_processed.csv")
-        data.to_csv(processed_file_path, index=False)
-        return processed_file_path  # Return path of processed file
+        # processed_file_path = file_path.replace(".log", "_processed.csv")
+        # data.to_csv(processed_file_path, index=False)
+        # return processed_file_path  # Return path of processed file
 
-        '''
-
-        # Path to the log file
-        # log_file = 'path_to_log_file.log'
-        #log_file = '/app/temp/marcajes_original.log'
-        log_file = '/app/temp/marcajes_original.log'
-
-        # Read the log file into a pandas DataFrame assuming it's comma-separated
-        df = pd.read_csv(log_file, header=None, encoding='utf-8')
-        # Assign the new column names as per your desired output
-        df.columns = ['codigo', 'entrada_salida', 'rut', 'hora', 'minuto', 'mes', 'dia', 'anho', 'accion']
+        
 
         # Save the DataFrame to a CSV file
-        df.to_csv('/app/temp/datos_procesados.csv', index=False, encoding='utf-8')
+        data.to_csv('/app/temp/datos_procesados.csv', index=False, encoding='utf-8')
         
-        processed_file_path = '/app/temp/datos_procesados.csv'
+        # processed_file_path = '/app/temp/datos_procesados.csv'
 
         # Optionally, print the DataFrame to verify
         # print(df.head())
-        return processed_file_path  # Return path of processed file
+        # return processed_file_path  # Return path of processed file
     
     except Exception as e:
         print(f"Error processing file: {e}")
