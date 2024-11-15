f4db91e8046dc25ec8558cfac240b403c463dd72
Daniel Rojas Alday <102681651+danielestebanRA@users.noreply.github.com>
2024-11-15 01:29:02 -0300

    update

--- test/test_carga_archivo.py
+++ test/test_carga_archivo.py
@@ -1,4 +1,7 @@
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from app import app  # Asegúrate de que `app.py` y `test_app.py` estén en el mismo directorio
 
